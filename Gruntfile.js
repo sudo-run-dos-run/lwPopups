faa92b53c092da245eb6659975317018be72b188
Tobias Weigl <github@tobias-weigl.de>
2022-12-06 20:55:28 +0100

    Build: We add Jasmine as a testing tool and integrate it into the default build task pipeline. As an example, we add 2 simple test cases for a utility function in lwPopupsUtilities.js. Those are unit tests, now we still need integration tests for runtime DOM testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+      files: ['Gruntfile.js', 'src/**/*.js']
     },
 	uglify: {
 	  my_target: {
@@ -11,6 +11,18 @@ module.exports = function(grunt) {
 		}
 	  }
 	},
+    jasmine: {
+      options: {
+          version: '3.8.0',
+          noSandbox: true
+      },
+      myTest001: {
+        src: 'src/**/*.js',
+        options: {
+          specs: 'test/*.test.js'
+        }
+      }
+    },
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
@@ -20,7 +32,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
-  grunt.registerTask('default', ['jshint', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'uglify', 'jasmine']);
 
 };
